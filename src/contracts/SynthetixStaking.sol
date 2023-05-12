712d37b6a3e1f2da193b80fbc11c8aea18c8a01f
0xvalerius <0xvalerius@proton.me>
2023-05-12 17:30:00 +0200

    Added @notice to the SynthetixStaking.sol contract.

--- src/contracts/SynthetixStaking.sol
+++ src/contracts/SynthetixStaking.sol
@@ -32,10 +32,12 @@ contract SynthetixStaking is Ownable {
 
     /* ========== VIEWS ========== */
 
+    /// @notice Returns the last time rewards were applicable.
     function lastTimeRewardApplicable() public view returns (uint256) {
         return block.timestamp < endAt ? block.timestamp : endAt;
     }
 
+    /// @notice Returns the reward per token earned by staking until the last time rewards were applicable.
     function rewardPerToken() public view returns (uint256) {
         if (totalSupply == 0) {
             return rewardPerTokenStored;
@@ -43,12 +45,14 @@ contract SynthetixStaking is Ownable {
         return rewardPerTokenStored + ((lastTimeRewardApplicable() - lastUpdateTime) * rewardRate * 1e18) / totalSupply;
     }
 
+    /// @notice Returns the amount of rewards earned by an account.
     function earned(address account) public view returns (uint256) {
         return (balances[account] * (rewardPerToken() - userRewardPerTokenPaid[account])) / 1e18 + rewards[account];
     }
 
     /* ========== MODIFIERS ========== */
 
+    /// @notice Updates the reward variables for an account before executing a function.
     modifier updateReward(address account) {
         rewardPerTokenStored = rewardPerToken();
         lastUpdateTime = lastTimeRewardApplicable();
@@ -61,6 +65,7 @@ contract SynthetixStaking is Ownable {
 
     /* ========== MUTATIVE FUNCTIONS ========== */
 
+    /// @notice Allows an account to stake tokens and earn rewards.
     function stake(uint256 amount) external updateReward(msg.sender) {
         require(amount > 0, "Cannot stake 0");
         totalSupply += amount;
@@ -69,6 +74,7 @@ contract SynthetixStaking is Ownable {
         emit Staked(msg.sender, amount);
     }
 
+    /// @notice Allows an account to withdraw their staked tokens and claim their rewards.
     function withdraw(uint256 amount) public updateReward(msg.sender) {
         require(amount > 0, "Cannot withdraw 0");
         totalSupply -= amount;
@@ -77,6 +83,7 @@ contract SynthetixStaking is Ownable {
         emit Withdrawn(msg.sender, amount);
     }
 
+    /// @notice Allows an account to claim their rewards without withdrawing their staked tokens.
     function getReward() public updateReward(msg.sender) {
         uint256 reward = rewards[msg.sender];
         if (reward > 0) {
@@ -86,6 +93,7 @@ contract SynthetixStaking is Ownable {
         }
     }
 
+    /// @notice Allows an account to withdraw their staked tokens and claim their rewards in a single transaction.
     function exit() external {
         withdraw(balances[msg.sender]);
         getReward();
@@ -93,6 +101,9 @@ contract SynthetixStaking is Ownable {
 
     /* ========== ONLYOWNER FUNCTIONS ========== */
 
+    /// @notice Notifies the contract that a reward has been added for the current reward period. *
+    /// It updates the reward rate and the last time rewards were applicable based on the amount of the reward and the duration of the reward period. *
+    /// This function can only be called by the contract owner.
     function notifyRewardAmount(uint256 reward) external onlyOwner updateReward(address(0)) {
         if (block.timestamp >= endAt) {
             rewardRate = reward / rewardsDuration;
@@ -110,6 +121,7 @@ contract SynthetixStaking is Ownable {
         emit RewardAdded(reward);
     }
 
+    /// @notice Allows the contract owner to change the duration of the reward period.
     function setRewardsDuration(uint256 _rewardsDuration) external onlyOwner {
         require(
             block.timestamp > endAt,
@@ -119,6 +131,7 @@ contract SynthetixStaking is Ownable {
         emit RewardsDurationUpdated(rewardsDuration);
     }
 
+    /// @notice Allows the contract owner to recover any ERC20 token sent to the contract in error except for the staking and reward tokens.
     function recoverERC20(address tokenAddress, uint256 tokenAmount) external onlyOwner {
         require(
             tokenAddress != address(stakingToken) && tokenAddress != address(rewardToken),
