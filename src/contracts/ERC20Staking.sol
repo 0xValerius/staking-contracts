48c6c40c091b216412521897b6c9b5fa517d724d
0xvalerius <0xvalerius@proton.me>
2023-05-14 23:16:56 +0200

    ERC20Staking.sol: added STATE VARIABLES.

--- src/contracts/ERC20Staking.sol
+++ src/contracts/ERC20Staking.sol
@@ -5,4 +5,21 @@ pragma solidity ^0.8.17;
 import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
 import {Ownable} from "openzeppelin/access/Ownable.sol";
 
-contract ERC20Staking is Ownable {}
+contract ERC20Staking is Ownable {
+    /* ========== STATE VARIABLES ========== */
+
+    IERC20 public immutable stakingToken;
+    IERC20 public immutable rewardToken;
+
+    uint256 public startAt;
+    uint256 public endAt;
+    uint256 public rewardRate;
+    uint256 public rewardsDuration;
+    uint256 public rewardPerTokenStored;
+
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
+
+    uint256 public totalSupply;
+    mapping(address => uint256) public balances;
+}
