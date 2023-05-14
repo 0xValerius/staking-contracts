9034f919a23f92485407131ab9999bee070e2eee
0xvalerius <0xvalerius@proton.me>
2023-05-14 23:09:57 +0200

    ERC20Staking.sol: initial commit.

---
+++ src/contracts/ERC20Staking.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
+import {Ownable} from "openzeppelin/access/Ownable.sol";
+
+contract ERC20Staking is Ownable {}
