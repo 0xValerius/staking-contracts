bcbeac5010cd48f38af15597ba33af2d0137b52f
0xvalerius <0xvalerius@proton.me>
2023-05-18 18:00:00 +0200

    ERC20Staking.sol: added reward tracking variables, completed updateRewardAllocation().

--- src/contracts/ERC20Staking.sol
+++ src/contracts/ERC20Staking.sol
@@ -12,19 +12,66 @@ contract ERC20Staking is Ownable {
 
     uint256 public startAt;
     uint256 public endAt;
+    uint256 public duration;
     uint256 public rewardRate;
-    uint256 public rewardsDuration;
     uint256 public rewardPerTokenStored;
+    uint256 public lastUpdateTime;
 
     mapping(address => uint256) public userRewardPerTokenPaid;
     mapping(address => uint256) public rewards;
 
-    uint256 public totalSupply;
+    // balance of this contract = toDistributeRewards + owedRewards
+
+    uint256 public totalStaked;
+    uint256 public toDistributeRewards;
+    uint256 public owedRewards;
     mapping(address => uint256) public balances;
 
     /* ========== CONSTRUCTOR ========== */
     constructor(address _stakingToken, address _rewardToken) {
         stakingToken = IERC20(_stakingToken);
         rewardToken = IERC20(_rewardToken);
+    }
+
+    /* ========== ADMIN FUNCTIONS ========== */
+    /// @notice Set starting timestamp of the staking period.
+    function setStartAt(uint256 _startAt) external onlyOwner {
+        require(startAt == 0, "Cannot set startAt twice");
+        require(_startAt > block.timestamp, "Cannot set startAt in the past");
+        startAt = _startAt;
+    }
+
+    /// @notice Set ending timestamp of the staking period. Update duration.
+    function setEndAt(uint256 _endAt) external onlyOwner {
+        require(endAt >= block.timestamp, "Cannot set endAt in the past");
+        require(_endAt > startAt, "Cannot set endAt before startAt");
+        endAt = _endAt;
+        duration = endAt - startAt;
+    }
+
+    /// @notice Update reward allocation.
+    function updateRewardAllocation(uint256 reward) external onlyOwner {
+        require(endAt >= block.timestamp, "Cannot update reward allocation after endAt");
+        require(startAt > 0, "Cannot update reward allocation before startAt");
+        require(reward > 0, "Cannot update reward allocation to zero");
+
+        require(
+            rewardToken.balanceOf(address(this)) >= toDistributeRewards + reward + owedRewards,
+            "Cannot update reward allocation to more than the balance of the contract"
+        );
+
+        toDistributeRewards += reward;
+        rewardRate = toDistributeRewards / duration;
+    }
+
+    /// @notice Allows the contract owner to recover any ERC20 token sent to the contract in error except for the staking and reward tokens.
+    // TO-DO: remove excess reward tokens from the contract
+    function recoverERC20(address tokenAddress, uint256 tokenAmount) external onlyOwner {
+        require(
+            tokenAddress != address(stakingToken) && tokenAddress != address(rewardToken),
+            "Cannot withdraw the staking or reward tokens"
+        );
+        IERC20(tokenAddress).transfer(msg.sender, tokenAmount);
+        //emit Recovered(tokenAddress, tokenAmount);
     }
 }
