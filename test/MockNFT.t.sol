8b810501c92b390dca0189655f47f6ea0cc1c115
0xvalerius <0xvalerius@proton.me>
2023-06-02 12:16:10 +0200

    Change of nomeclature.

--- test/MockNFT.t.sol
+++ test/MockNFT.t.sol
@@ -6,30 +6,30 @@ import "forge-std/Test.sol";
 import {MockNFT} from "../src/contracts/MockNFT.sol";
 
 contract MockNFTTest is Test {
-    MockNFT nft;
+    MockNFT collection;
     address actor1 = address(0x1);
     address actor2 = address(0x2);
 
     function setUp() public {
-        nft = new MockNFT("MockNFT", "MNFT");
+        collection = new MockNFT("MockNFT", "MNFT");
     }
 
     function test_MockNFTDeploy() public {
-        assertEq(nft.name(), "MockNFT");
-        assertEq(nft.symbol(), "MNFT");
+        assertEq(collection.name(), "MockNFT");
+        assertEq(collection.symbol(), "MNFT");
     }
 
     function test_MockNFTMint() public {
-        nft.mint(actor1, 1);
-        assertEq(nft.balanceOf(actor1), 1);
-        assertEq(nft.totalSupply(), 1);
+        collection.mint(actor1, 1);
+        assertEq(collection.balanceOf(actor1), 1);
+        assertEq(collection.totalSupply(), 1);
     }
 
     function test_MockNFTTransfer() public {
-        nft.mint(actor1, 1);
+        collection.mint(actor1, 1);
         vm.prank(actor1);
-        nft.transferFrom(actor1, actor2, 0);
-        assertEq(nft.balanceOf(actor1), 0);
-        assertEq(nft.balanceOf(actor2), 1);
+        collection.transferFrom(actor1, actor2, 0);
+        assertEq(collection.balanceOf(actor1), 0);
+        assertEq(collection.balanceOf(actor2), 1);
     }
 }
